c24bd830f9a0cca7ee977298fe960258020098b7
Breaker <791628659@qq.com>
2018-02-17 20:23:16 +0800

    面向对象

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
+class Tools(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.move(100, 100)                             # 位置
+        self.resize(100, 100)                           # 窗口大小
+        self.setWindowTitle("This is a title")          # 标题
+        self.show()                                     # 显示
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.move(100, 100)
-    w.setWindowTitle("This is a title")
-    w.resize(100, 100)
-    w.show()
-
+    tools = Tools()
     sys.exit(app.exec_())
